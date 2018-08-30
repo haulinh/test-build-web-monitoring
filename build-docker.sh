b35cc07e311ce53b2bf1833f7314b4df9529cf0d
duong.nguyen@vietan-software.com <duong.nguyen@vietan-software.com>
2018-08-30 09:26:36 +0700

    docker envisoft

--- build-docker.sh
+++ build-docker.sh
@@ -1,4 +1,4 @@
-docker build -t ilotusenviro-web-gis .
-docker tag ilotusenviro-web-gis today108/ilotusenviro-web-gis
-docker push today108/ilotusenviro-web-gis
+docker build -t envisoft-web-gis .
+docker tag envisoft-web-gis today108/envisoft-web-gis
+docker push today108/envisoft-web-gis
 echo "success deploy web gis"
