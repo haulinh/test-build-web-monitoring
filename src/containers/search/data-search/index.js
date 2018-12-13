fbf3a3f85f81998fd61ff2758f67d67881ac24bf
Doanh Trinh <doannh.trinh@vietan-software.com>
2018-12-13 16:23:40 +0700

    fixbug-dev-doanh

--- src/containers/search/data-search/index.js
+++ src/containers/search/data-search/index.js
@@ -65,7 +65,6 @@ export default class MinutesDataSearch extends React.Component {
       searchFormData
     )
 
-    console.log('dataAnalyzeStationAuto: ', dataAnalyzeStationAuto)
     this.setState({
       isLoading: false,
       dataAnalyzeStationAuto: dataAnalyzeStationAuto.success
