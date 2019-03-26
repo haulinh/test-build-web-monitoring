5c626f59a97c39e34aabcf121039a256c72e7726
dtanphat9388 <dtanphat9388@gmail.com>
2019-03-26 14:24:55 +0700

    Updated file:         - build-docker.sh: Optimized config         - .gitignore Removed files:         - run_server.sh

--- build-docker.sh
+++ build-docker.sh
@@ -1,4 +1,3 @@
-docker build -t envisoft-web-gis .
-docker tag envisoft-web-gis today108/envisoft-web-gis
-docker push today108/envisoft-web-gis
+docker build . -t ilotusland/envisoft-web-gis:dev
+docker push ilotusland/envisoft-web-gis:dev
 echo "success deploy web gis"
