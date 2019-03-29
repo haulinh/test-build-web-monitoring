9bb4c2cc5aa887821e1907a4f95731c3b3eee593
dtanphat9388 <dtanphat9388@gmail.com>
2019-03-29 17:56:14 +0700

    change docker image name

--- build-docker.sh
+++ build-docker.sh
@@ -1,3 +1,3 @@
-docker build . -t ilotusland/envisoft-web-gis:dev
-docker push ilotusland/envisoft-web-gis:dev
+docker build . -t ilotusland/enviro-web-gis:dev
+docker push ilotusland/enviro-web-gis:dev
 echo "success deploy web gis"
