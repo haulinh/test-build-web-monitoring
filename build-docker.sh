ef7d0f47a4ffdcec2e60fda65e8a83c689b8e989
dtanphat9388 <dtanphat9388@gmail.com>
2019-04-01 23:46:44 +0700

    change docker image tag

--- build-docker.sh
+++ build-docker.sh
@@ -1,3 +1,3 @@
-docker build . -t ilotusland/enviro-web-gis:dev
-docker push ilotusland/enviro-web-gis:dev
+docker build . -t ilotusland/enviro-web-gis:prod
+docker push ilotusland/enviro-web-gis:prod
 echo "success deploy web gis"
