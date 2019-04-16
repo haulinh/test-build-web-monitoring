a2ec28df8f53b5d8954b5bef62fd6a86e51b64c8
sevenlure <01226905092>
2019-04-16 15:45:51 +0700

    commit

--- src/components/monitoring/station-type-group/station-type-item/station-auto/measuring/measuring-item/index.js
+++ src/components/monitoring/station-type-group/station-type-item/station-auto/measuring/measuring-item/index.js
@@ -1,37 +1,37 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { autobind } from 'core-decorators'
-import styled from 'styled-components'
-import { colorLevels } from 'constants/warningLevels'
-import { translate } from 'hoc/create-lang'
-import { Tooltip } from 'antd'
-import { get } from 'lodash'
+import React from "react";
+import PropTypes from "prop-types";
+import { autobind } from "core-decorators";
+import styled from "styled-components";
+import { colorLevels } from "constants/warningLevels";
+import { translate } from "hoc/create-lang";
+import { Tooltip } from "antd";
+import { get } from "lodash";
 
 const DEVICE_STATUS = {
-  '0': { src: '/images/sensor1.png', text: 'monitoring.deviceStatus.normal' },
-  '1': {
-    src: '/images/sensor2.png',
-    text: 'monitoring.deviceStatus.maintenance'
+  "0": { src: "/images/sensor1.png", text: "monitoring.deviceStatus.normal" },
+  "1": {
+    src: "/images/sensor2.png",
+    text: "monitoring.deviceStatus.maintenance"
   },
-  '2': { src: '/images/sensor3.png', text: 'monitoring.deviceStatus.broken' }
-}
+  "2": { src: "/images/sensor3.png", text: "monitoring.deviceStatus.broken" }
+};
 
 const MeasuringItemWrapper = styled.div`
   position: relative;
   display: flex;
   padding: 8px 8px;
-  flex-direction: column;
+  // flex-direction: column;
   justify-content: space-between;
   border-radius: 8px;
   border: solid 1px ${props => props.color};
   &:hover {
     cursor: pointer;
   }
-`
+`;
 const MeasuringItemText = styled.div`
   display: flex;
-  justify-content: space-between;
-`
+  // justify-content: space-between;
+`;
 
 const MeasuringName = styled.span`
   padding: 0px 6px;
@@ -42,27 +42,45 @@ const MeasuringName = styled.span`
   justify-content: center;
   border-radius: 3px;
   background-color: ${props => props.color};
-`
+`;
 
 const MeasuringUnit = styled.span`
   position: relative;
   top: -10px;
   font-size: 8px;
   color: ${props => props.color};
-`
+`;
 
 const MeasuringValue = styled.div`
   font-size: 16px;
   text-align: left;
-  font-weight: 600;
+  // font-weight: 600;
   color: ${props => props.color};
   position: relative;
-`
+`;
 
 const MeasuringLimit = styled.span`
   font-size: 10px;
   color: #b9b9b9;
-`
+`;
+
+const LeftContainer = styled.div`
+  // display: flex;
+  // flex: 1;
+`;
+const RightContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Dot = styled.div`
+  height: 48px;
+  width: 48px;
+  background-color: #bbb;
+  border-radius: 50%;
+  display: inline-block;
+  margin-right: 8px;
+`;
 
 @autobind
 export default class MeasuringItem extends React.PureComponent {
@@ -74,27 +92,27 @@ export default class MeasuringItem extends React.PureComponent {
     minLimit: PropTypes.number,
     maxLimit: PropTypes.number,
     warningLevel: PropTypes.string
-  }
+  };
 
   getLimitText() {
-    const { unit } = this.props
-    let minLimit = get(this.props, 'minLimit', null)
-    let maxLimit = get(this.props, 'maxLimit', null)
-    if (minLimit === '') minLimit = null
-    if (maxLimit === '') maxLimit = null
+    const { unit } = this.props;
+    let minLimit = get(this.props, "minLimit", null);
+    let maxLimit = get(this.props, "maxLimit", null);
+    if (minLimit === "") minLimit = null;
+    if (maxLimit === "") maxLimit = null;
 
     if (minLimit !== null && maxLimit !== null) {
       return ` ${translate(
-        'monitoring.limit'
-      )}: ${minLimit} -> ${maxLimit} ${unit || ''}`
+        "monitoring.limit"
+      )}: ${minLimit} -> ${maxLimit} ${unit || ""}`;
     }
 
     if (minLimit !== null) {
-      return ` ${translate('monitoring.limit')}: > ${minLimit}  ${unit || ''}`
+      return ` ${translate("monitoring.limit")}: > ${minLimit}  ${unit || ""}`;
     }
 
     if (maxLimit !== null) {
-      return `${translate('monitoring.limit')}: < ${maxLimit}  ${unit || ''}`
+      return `${translate("monitoring.limit")}: < ${maxLimit}  ${unit || ""}`;
     }
 
     // if (minLimit || maxLimit) {
@@ -105,48 +123,74 @@ export default class MeasuringItem extends React.PureComponent {
     //     else limitText = translate('monitoring.limit') + ': < ' + maxLimit
     //   }
     // }
-    return `${translate('monitoring.limit')} `
+    return `${translate("monitoring.limit")} `;
     //return limitText ? `${limitText} ${unit}` : ` `
   }
 
   getColorLevel() {
-    const { warningLevel } = this.props
+    const { warningLevel } = this.props;
     if (warningLevel && colorLevels[warningLevel])
-      return colorLevels[warningLevel]
-    return colorLevels.GOOD
+      return colorLevels[warningLevel];
+    return colorLevels.GOOD;
   }
 
   renderDeviceIcon = status => {
-    let item = DEVICE_STATUS[`${status}`]
+    let item = DEVICE_STATUS[`${status}`];
     if (item) {
       return (
         <Tooltip placement="top" title={`Sensor ${translate(item.text)}`}>
           <img
             src={item.src}
             style={{
-              position: 'absolute',
+              position: "absolute",
               bottom: 4,
               right: 8,
-              width: '16px',
-              height: '16px'
+              width: "16px",
+              height: "16px"
             }}
             alt={item.text}
           />
         </Tooltip>
-      )
+      );
     }
 
-    return null
-  }
+    return null;
+  };
 
   render() {
-    const { value, unit, name } = this.props
+    const { value, unit, name } = this.props;
     return (
       <MeasuringItemWrapper
         onClick={this.props.onClick}
         color={this.getColorLevel()}
       >
-        <MeasuringItemText>
+        <LeftContainer>
+          <MeasuringItemText
+            style={{ color: this.getColorLevel(), marginTop: 4, fontSize: 16 }}
+          >
+            <span style={{ marginRight: 8, fontWeight: 100 }}>{name}</span>
+            <MeasuringValue color={this.getColorLevel()}>
+              {value !== undefined
+                ? value.toLocaleString(navigator.language)
+                : ""}{" "}
+              {unit ? (
+                <MeasuringUnit color={this.getColorLevel()} className="unit">
+                  {unit}
+                </MeasuringUnit>
+              ) : (
+                ""
+              )}
+            </MeasuringValue>
+          </MeasuringItemText>
+          <div style={{ marginTop: 4 }}>
+            <MeasuringLimit>{this.getLimitText()}</MeasuringLimit>
+          </div>
+        </LeftContainer>
+        <RightContainer>
+          <Dot />
+        </RightContainer>
+
+        {/* <MeasuringItemText>
           <MeasuringValue color={this.getColorLevel()}>
             {value !== undefined
               ? value.toLocaleString(navigator.language)
@@ -160,10 +204,10 @@ export default class MeasuringItem extends React.PureComponent {
             )}
           </MeasuringValue>
           <MeasuringName color={this.getColorLevel()}>{name}</MeasuringName>
-        </MeasuringItemText>
-        <MeasuringLimit>{this.getLimitText()}</MeasuringLimit>
-        {this.renderDeviceIcon(get(this.props, 'statusDevice', ''))}
+        </MeasuringItemText> */}
+        {/* <MeasuringLimit>{this.getLimitText()}</MeasuringLimit>
+        {this.renderDeviceIcon(get(this.props, 'statusDevice', ''))} */}
       </MeasuringItemWrapper>
-    )
+    );
   }
 }
