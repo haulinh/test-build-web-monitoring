b63988688d42b7e6c035e63141a0e779d63974e8
Mai Thuận Thảo <thao.mai@vietan-software.com>
2019-04-24 13:09:37 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -32,7 +32,7 @@ export function getConfigApi() {
     fcmMessages: c('fcm-messages'),
     organization: c('organization'),
     role: c('role'),
-    sampling: 'http://192.168.11.104:5018/samplingNew',
+    sampling: c('samplingNew'),
     // sampling: c('sampling'),
     support: c('support'),
     province: c('province'),
