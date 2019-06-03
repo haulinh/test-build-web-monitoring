73cfa1a3e340c3a19c57c128db48977a8b13d4c5
dtanphat9388 <dtanphat9388@gmail.com>
2019-06-03 11:30:21 +0700

    change docker container registry to azure

--- build-docker.sh
+++ build-docker.sh
@@ -1,10 +1,13 @@
 # đoạn script này sẽ build ra docker image,
 # với name là DOCKER_IMAGE và tag của docker image chính là git branch name
 #-----------------------------------------------------------------------
+REGISTRY=ilotusland.azurecr.io
 DOCKER_IMAGE=ilotusland/enviro-web-gis
 BRAND_NAME=$(git symbolic-ref --short HEAD)
 
-docker build . -t ${DOCKER_IMAGE}:${BRAND_NAME}
-docker push ${DOCKER_IMAGE}:${BRAND_NAME}
-echo --------------------------------------------------
-echo "success deploy with name ${DOCKER_IMAGE}:${BRAND_NAME}"
+#-----------------------------------------------------------------------
+docker build . -t ${REGISTRY}/${DOCKER_IMAGE}:${BRAND_NAME}
+docker push ${REGISTRY}/${DOCKER_IMAGE}:${BRAND_NAME}
+
+echo -----------------------------------------------------------------------
+echo Image sau khi build thành công sẽ có tên: ${REGISTRY}/${DOCKER_IMAGE}:${BRAND_NAME}
