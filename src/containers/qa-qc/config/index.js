0cf8858a94e4f9cccb1ae10e93fa1ae0943620ea
sevenlure <01226905092>
2019-06-05 09:27:04 +0700

    push code để dừng lại hỗ trợ anh Thọ build version

--- src/containers/qa-qc/config/index.js
+++ src/containers/qa-qc/config/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Checkbox, Form, Button, Tabs, Card } from "antd";
+import { Checkbox, Form, Button, Tabs, Card, Skeleton } from "antd";
 import PageContainer from "layout/default-sidebar-layout/PageContainer";
 import Breadcrumb from "../breadcrumb";
 import { translate } from "hoc/create-lang";
@@ -7,6 +7,7 @@ import { getStationTypes } from "api/CategoryApi";
 import * as _ from "lodash";
 import { connect } from "react-redux";
 import TableConfig from "./table";
+import { getConfigQAQC, putConfigQAQC } from "api/CategoryApi";
 
 const { TabPane } = Tabs;
 
@@ -18,27 +19,50 @@ const i18n = {
   btnSave: translate("addon.save")
 };
 
-
 @connect(state => ({
   isInitLoaded: state.stationAuto.isInitLoaded,
   stationList: state.stationAuto.list
 }))
 @Form.create()
 export default class QAQC_Config extends React.Component {
-  state = {
-    isInitLoaded: false,
-    activeTabkey: "tab1",
-    tabList: []
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      isInitLoaded: false,
+      activeTabkey: "tab1",
+      tabList: []
+    };
+    this.getData = this.getData.bind(this);
+  }
 
-  handleSubmit = e => {
-    e.preventDefault();
-    this.props.form.validateFields((err, values) => {
+  dataTable = [];
+
+  handleSubmit = () => {
+    console.log("aaa");
+    this.props.form.validateFields(async (err, values) => {
+      console.log("aaa");
       if (!err) {
         console.log("Received values of form: ", values);
       }
+      let measureConfig = await this.getData();
+      console.log("measureConfig", measureConfig);
+      let response = await putConfigQAQC({
+        ...values,
+        measureConfig
+      });
+      console.log("response", response);
     });
   };
+
+  async getData() {
+    let result = {};
+    for (var i = 0; i < this.dataTable.length; i++) {
+      let item = this.dataTable[i];
+      let data = await item.getTableData();
+      result = _.merge(result, data);
+    }
+    return result;
+  }
 
   onTabChange = (key, type) => {
     console.log(key, type);
@@ -61,7 +85,36 @@ export default class QAQC_Config extends React.Component {
       });
     }
 
-    this.setState({ isInitLoaded: true });
+    let response = await getConfigQAQC();
+
+    let dataForm = {};
+    if (response.success) {
+      const { data } = response;
+      console.log("data,", data);
+      // const { setFieldsValue } = this.props.form;
+
+      // setTimeout(()=>{
+
+      // },5000)
+      // setFieldsValue({
+      //   beyondMeasuringRange: data.beyondMeasuringRange,
+      //   deviceError: data.deviceError,
+      //   deviceCalibration: data.deviceCalibration,
+      //   ...data.measureConfig
+      // })
+      dataForm = {
+        beyondMeasuringRange: data.beyondMeasuringRange,
+        deviceError: data.deviceError,
+        deviceCalibration: data.deviceCalibration,
+        ...data.measureConfig
+      };
+    }
+    // console.log('datqa',data)
+
+    this.setState({ isInitLoaded: true }, () => {
+      const { setFieldsValue } = this.props.form;
+      setFieldsValue(dataForm);
+    });
   }
 
   getMeasuring() {
@@ -104,22 +157,33 @@ export default class QAQC_Config extends React.Component {
           backgroundColor={"#fafbfb"}
         />
         <Breadcrumb items={["configNew"]} />
-        <Form style={{ padding: 24 }} onSubmit={this.handleSubmit}>
-          <Form.Item style={{ marginBottom: 8 }}>
-            {getFieldDecorator("beyondMeasuringRange", {})(
-              <Checkbox>{i18n.beyondMeasuringRange}</Checkbox>
-            )}
-          </Form.Item>
-          <Form.Item style={{ marginBottom: 8 }}>
-            {getFieldDecorator("deviceError", {})(
-              <Checkbox>{i18n.deviceError}</Checkbox>
-            )}
-          </Form.Item>
-          <Form.Item style={{ marginBottom: 8 }}>
-            {getFieldDecorator("deviceCalibration", {})(
-              <Checkbox>{i18n.deviceCalibration}</Checkbox>
-            )}
-          </Form.Item>
+        <Form style={{ padding: 24 }}>
+          {!this.state.isInitLoaded && (
+            <Skeleton
+              title={false}
+              paragraph={{ rows: 3, width: [200, 200, 200] }}
+            />
+          )}
+
+          {this.state.isInitLoaded && (
+            <div>
+              <Form.Item style={{ marginBottom: 8 }}>
+                {getFieldDecorator("beyondMeasuringRange", {
+                  valuePropName: "checked"
+                })(<Checkbox>{i18n.beyondMeasuringRange}</Checkbox>)}
+              </Form.Item>
+              <Form.Item style={{ marginBottom: 8 }}>
+                {getFieldDecorator("deviceError", {
+                  valuePropName: "checked"
+                })(<Checkbox>{i18n.deviceError}</Checkbox>)}
+              </Form.Item>
+              <Form.Item style={{ marginBottom: 8 }}>
+                {getFieldDecorator("deviceCalibration", {
+                  valuePropName: "checked"
+                })(<Checkbox>{i18n.deviceCalibration}</Checkbox>)}
+              </Form.Item>
+            </div>
+          )}
 
           <Card
             loading={!this.state.isInitLoaded || !this.props.isInitLoaded}
@@ -137,9 +201,10 @@ export default class QAQC_Config extends React.Component {
                   };
                 });
                 return (
-                  <TabPane tab={tab.name} key={tab.key}>
+                  <TabPane forceRender={true} tab={tab.name} key={tab.key}>
                     <TableConfig
-                      getRef={ref => (this.TableConfig = ref)}
+                      form={this.props.form}
+                      getRef={ref => this.dataTable.push(ref)}
                       dataTableMeasures={dataTableMeasures}
                       type={tab.key}
                     />
@@ -150,14 +215,7 @@ export default class QAQC_Config extends React.Component {
           </Card>
 
           <br />
-          <Button
-            block
-            type="primary"
-            onClick={e => {
-              e.preventDefault();
-              console.log("this.refs", this.TableConfig.getTableData());
-            }}
-          >
+          <Button block type="primary" onClick={this.handleSubmit}>
             {i18n.btnSave}
           </Button>
         </Form>
