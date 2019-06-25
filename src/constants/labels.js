b0ff046c2aabac9378060fa7a5245487d4afa0d8
dtanphat9388 <dtanphat9388@gmail.com>
2019-06-25 11:15:57 +0700

    add labels

--- src/constants/labels.js
+++ src/constants/labels.js
@@ -6,3 +6,10 @@ export const STATION_AUTO_OPTIONS = {
   sms: 'sms',
   email: 'email'
 }
+
+export const USER_RULE_TABLE_OPTIONS = {
+  manager: "manager",
+  warning: 'warning',
+  sms: 'sms',
+  email: 'email'
+}
