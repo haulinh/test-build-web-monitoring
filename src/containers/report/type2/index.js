6386cd030054837a9d965387ad57f171d6c65d04
dtanphat9388 <dtanphat9388@gmail.com>
2019-07-15 16:41:42 +0700

    Tích họp UI Báo Cáo Type 2

---
+++ src/containers/report/type2/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import PageContainer from "layout/default-sidebar-layout/PageContainer";
+import Breadcrumb from "../breadcrumb";
+import SearchForm from "./search-form";
+import {getUrlReportType2} from 'api/DataStationAutoApi'
+import { connect } from 'react-redux'
+import axios from 'axios'
+
+@connect(state => ({
+  token: state.auth.token
+}))
+export default class ReportType2 extends React.Component {
+  constructor(props){
+    super(props)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  handleSubmit(values){
+    let measuringListUnitStr = ''
+    if(values.measuringList) measuringListUnitStr = values.measuringList.map(item=> encodeURIComponent(item.unit)).join(',')
+    
+    let measuringListStr = ''
+    if(values.measuringList) measuringListStr = values.measuringList.map(item=> encodeURIComponent(item.key)).join(',')
+    let url = getUrlReportType2(this.props.token, values.stationAuto, values.time.format("MM-YYYY"), measuringListStr, measuringListUnitStr)
+    console.log('getUrlReportType1', url)
+    // window.location.href = url
+    window.open(url)
+  }
+  render() {
+ 
+    return (
+      <PageContainer>
+        <Breadcrumb items={["type2"]} />
+        <SearchForm cbSubmit={this.handleSubmit} />
+      </PageContainer>
+    );
+  }
+}
