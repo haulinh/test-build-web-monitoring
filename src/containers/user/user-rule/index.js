f71fb581c792b1259aafe5ab462a70eeea73e1b3
Duong Tan Phat <dtanphat9388@gmail.com>
2019-08-08 12:00:25 +0700

    fixed pages trong menu config

--- src/containers/user/user-rule/index.js
+++ src/containers/user/user-rule/index.js
@@ -70,6 +70,10 @@ export default class StationAutoConfigNotification extends React.Component {
     isLoading: PropTypes.bool,
   }
 
+  static defaultProps = {
+    dataSource: []
+  }
+
   constructor(props) {
     super(props)
 
