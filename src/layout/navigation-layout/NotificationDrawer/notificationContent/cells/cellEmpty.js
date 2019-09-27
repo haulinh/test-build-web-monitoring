94bd90100c7f6ecdc8da23afcc6c2a5a5c345e22
Duong Tan Phat <dtanphat9388@gmail.com>
2019-09-27 13:36:37 +0700

    fixed eslint

--- src/layout/navigation-layout/NotificationDrawer/notificationContent/cells/cellEmpty.js
+++ src/layout/navigation-layout/NotificationDrawer/notificationContent/cells/cellEmpty.js
@@ -1,15 +1,4 @@
 import React from 'react'
-import _ from 'lodash'
-import { Row, Col, Card, Button} from 'antd'
-
-import { translate } from 'hoc/create-lang'
-
-const i18n = {
-  gotoRealtimeMonitoringPage: translate('actions.gotoMonitoring'),
-  viewDataAroundThisTime: translate('actions.viewDataAroundThisTime'),
-  exceeded: translate('stationStatus.exceeded'),
-  exceededPreparing: translate('stationStatus.exceededPreparing'),
-}
 
 export default function EmptyCell(props) {
   return <div></div>
