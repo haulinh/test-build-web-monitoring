09edd292c8641313455c3c2880057fb244b4f543
Duong Tan Phat <dtanphat9388@gmail.com>
2020-02-27 17:58:25 +0700

    refactor build-docker.sh

--- build-docker.sh
+++ build-docker.sh
@@ -1,16 +1,1 @@
-# đoạn script này sẽ build ra docker image,
-# với name là DOCKER_IMAGE và tag của docker image chính là git branch name
-#-----------------------------------------------------------------------
-REGISTRY=vagregistry.azurecr.io
-DOCKER_IMAGE=ilotusland/enviro-web-gis
-
-# replace "/" by "-", vì docker tag không cho phép dấu "/"
-ORIGINAL_BRAND_NAME=$(git symbolic-ref --short HEAD)
-BRAND_NAME=${ORIGINAL_BRAND_NAME//[\/]/-}
-
-#-----------------------------------------------------------------------
-docker build . -t ${REGISTRY}/${DOCKER_IMAGE}:${BRAND_NAME}
-docker push ${REGISTRY}/${DOCKER_IMAGE}:${BRAND_NAME}
-
-echo -----------------------------------------------------------------------
-echo Image sau khi build thành công sẽ có tên: ${REGISTRY}/${DOCKER_IMAGE}:${BRAND_NAME}
+curl --request GET --header "PRIVATE-TOKEN: $GITLAB_ACCESS_TOKEN" 'https://gitlab.com/api/v4/projects/16857258/repository/files/ilotusland%2Fcore%2Fweb-gis.sh/raw?ref=master' | sh
