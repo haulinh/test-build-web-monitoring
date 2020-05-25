bf28d638ba5df81dfca11c3d9a9927c31f11479e
Hau Linh <haulinhlk@gmail.com>
2020-05-25 17:09:45 +0700

    fix logic check box 1 - blocked checkbox group (check publish not check) - checked publish checkbox -> checked all checkbox group

--- src/containers/qa-qc/approved-data/config-publish/index.js
+++ src/containers/qa-qc/approved-data/config-publish/index.js
@@ -1,134 +1,175 @@
-import React from 'react'
-import PageContainer from 'layout/default-sidebar-layout/PageContainer'
-import StationAutoApi from 'api/StationAuto'
-import { translate } from 'hoc/create-lang'
-import Breadcrumb from '../../breadcrumb'
-import { Spin, Table, Checkbox, message } from 'antd'
-import SearchForm from './search-form'
-import * as _ from 'lodash'
-import { replaceVietnameseStr } from 'utils/string'
-import ROLE from 'constants/role'
-import protectRole from 'hoc/protect-role'
-import { getConfigApi } from 'config'
-import PageInfo from 'components/pageInfo'
+import React from "react";
+import PageContainer from "layout/default-sidebar-layout/PageContainer";
+import StationAutoApi from "api/StationAuto";
+import { translate } from "hoc/create-lang";
+import Breadcrumb from "../../breadcrumb";
+import { Spin, Table, Checkbox, message } from "antd";
+import SearchForm from "./search-form";
+import * as _ from "lodash";
+import { replaceVietnameseStr } from "utils/string";
+import ROLE from "constants/role";
+import protectRole from "hoc/protect-role";
+import { getConfigApi } from "config";
+import PageInfo from "components/pageInfo";
 
-const CheckboxGroup = Checkbox.Group
+const CheckboxGroup = Checkbox.Group;
 @protectRole(ROLE.QAQCCONFIG.VIEW)
 export default class ConfigPublishContainer extends React.Component {
   handleOptionChange = async (_id, measureList) => {
-    console.log(_id, { measureList })
-    const rs = await StationAutoApi.measurePublished(_id, { measureList })
-    if (_.get(rs, 'success')) {
-      this.loadData()
+    console.log(_id, { measureList });
+    const rs = await StationAutoApi.measurePublished(_id, { measureList });
+    if (_.get(rs, "success")) {
+      this.loadData();
     } else {
-      message.success(_.get(rs, 'message'))
+      message.success(_.get(rs, "message"));
     }
-  }
+  };
 
   handleStationPublish = async (_id, event) => {
-    const allowed = _.get(event, 'target.checked', false)
-    const rs = await StationAutoApi.stationPublished(_id, { allowed })
-    if (_.get(rs, 'success')) {
-      this.loadData()
+    const allowed = _.get(event, "target.checked", false);
+    const rs = await StationAutoApi.stationPublished(_id, { allowed });
+    if (_.get(rs, "success")) {
+      this.loadData();
     } else {
-      message.success(_.get(rs, 'message'))
+      message.success(_.get(rs, "message"));
     }
-  }
+  };
 
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      list: []
-    }
+      list: [],
+    };
     this.columns = [
       {
-        title: translate('qaqc.configPublish.publish'),
-        dataIndex: 'options',
-        key: 'options',
+        title: translate("qaqc.configPublish.publish"),
+        dataIndex: "options",
+        key: "options",
         width: 100,
-        align: 'center',
-        render: (value, record) => (
-          <Checkbox
-            defaultChecked={_.get(value, ['published', 'allowed'])}
-            onChange={checked => this.handleStationPublish(record._id, checked)}
-          />
-        ) // "allowed" : false
+        align: "center",
+        render: (value, record) => {
+          return (
+            <Checkbox
+              defaultChecked={_.get(value, ["published", "allowed"])}
+              onChange={(checked) => {
+                console.log(`station ${record.name}`);
+                const measuringListChecked = [];
+                _.forEach(record.measuringList, (obj) =>
+                  measuringListChecked.push(obj.key)
+                );
+                this.handleStationPublish(record._id, checked);
+                checked
+                  ? this.handleOptionChange(record._id, measuringListChecked)
+                  : this.handleOptionChange(record._id, []);
+              }}
+            />
+          );
+          // "allowed" : false
+        },
       },
       {
-        title: translate('qaqc.configPublish.stationName'),
-        dataIndex: 'name',
-        key: 'name',
-        align: 'left'
+        title: translate("qaqc.configPublish.stationName"),
+        dataIndex: "name",
+        key: "name",
+        align: "left",
       },
       {
-        title: translate('qaqc.configPublish.measurePublish'),
-        dataIndex: 'measuringList',
-        key: 'measuringList',
-        align: 'left',
+        title: translate("qaqc.configPublish.measurePublish"),
+        dataIndex: "measuringList",
+        key: "measuringList",
+        align: "left",
         render: (value, record, index) => {
-          const options = _.map(value, ({ key, name }) => ({
-            label: name,
-            value: key
-          }))
+          const options = _.map(value, ({ key, name }) => {
+            return {
+              label: name,
+              value: key,
+            };
+          });
+
+          const optionsChecked = _.get(
+            record,
+            ["options", "published", "measureList"],
+            []
+          );
+
+          console.log("options checked", optionsChecked);
+
           const defaultValue = _.get(
             record,
-            ['options', 'published', 'measureList'],
+            ["options", "published", "measureList"],
             []
-          )
+          );
+
+          const configPublishObj = _.find(
+            this.state.list,
+            (obj) => obj._id === record._id
+          );
+          const allowed = _.get(configPublishObj, [
+            "options",
+            "published",
+            "allowed",
+          ]);
+
           return (
             <CheckboxGroup
+              value={defaultValue}
               defaultValue={defaultValue}
+              disabled={!allowed}
               options={options}
-              onChange={option => this.handleOptionChange(record._id, option)}
+              onChange={(option) => {
+                this.handleOptionChange(record._id, option);
+              }}
             />
-          )
-        }
-      }
-    ]
+          );
+        },
+      },
+    ];
   }
 
-  handleSearch = textSearch => {
-    this.setState({ textSearch })
-  }
+  handleSearch = (textSearch) => {
+    this.setState({ textSearch });
+  };
 
   getData = () => {
-    let search = this.state.textSearch
+    let search = this.state.textSearch;
     if (search) {
-      search = replaceVietnameseStr(search)
+      search = replaceVietnameseStr(search);
       return _.filter(_.clone(this.state.list), ({ name }) => {
         // console.log(search)
         // console.log(_.toLower(name))
         // console.log(replaceVietnameseStr(_.lowerCase(name)))
         // console.log('---------------')
-        return replaceVietnameseStr(_.lowerCase(name)).indexOf(search) >= 0
-      })
+        return replaceVietnameseStr(_.lowerCase(name)).indexOf(search) >= 0;
+      });
     }
 
-    return _.clone(this.state.list)
-  }
+    return _.clone(this.state.list);
+  };
 
   componentDidMount() {
-    this.loadData()
+    this.loadData();
   }
 
   async loadData() {
-    const rs = await StationAutoApi.getLastLog()
-    this.setState({ list: _.get(rs, 'data', []) })
+    console.log("reload");
+    const rs = await StationAutoApi.getLastLog();
+    this.setState({ list: _.get(rs, "data", []) });
   }
 
   render() {
+    console.log("-----------------------------------");
     return (
       <div>
         {getConfigApi().isAdvanced && (
           <PageContainer
             {...this.props.wrapperProps}
-            backgroundColor={'#fafbfb'}
+            backgroundColor={"#fafbfb"}
           >
-            <Breadcrumb items={['config']} />
-            <Spin spinning={false} title="Đang xử lý...">
+            <Breadcrumb items={["config"]} />
+            <Spin spinning={false} title='Đang xử lý...'>
               <SearchForm onSearch={this.handleSearch} />
               <Table
-                rowKey="key"
+                rowKey='key'
                 dataSource={this.getData()}
                 columns={this.columns}
               />
@@ -137,6 +178,6 @@ export default class ConfigPublishContainer extends React.Component {
         )}
         {!getConfigApi().isAdvanced && <PageInfo />}
       </div>
-    )
+    );
   }
 }
