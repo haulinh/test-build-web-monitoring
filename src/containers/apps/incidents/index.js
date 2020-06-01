c144d47c1407a36981357e75fca37b779c756107
Tung Phan <tung.phan@vietan-software.com>
2020-06-01 16:55:04 +0700

    integration lang

--- src/containers/apps/incidents/index.js
+++ src/containers/apps/incidents/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Layout from 'layout/default-sidebar-layout'
 import { getApps } from 'config'
 import { translate } from 'hoc/create-lang'
+import { getLanguage } from 'utils/localStorage'
 
 export default class IncidentAppContainer extends React.Component {
   render() {
@@ -11,7 +12,7 @@ export default class IncidentAppContainer extends React.Component {
           title={translate('apps.incidents')}
           style={{ width: '100%', height: '100vh' }}
           frameBorder={0}
-          src={getApps().incidents}
+          src={`${getApps().incidents}?lng=${getLanguage()}`}
         >
         </iframe>
       </Layout>
