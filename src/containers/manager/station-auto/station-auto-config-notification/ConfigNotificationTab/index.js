e4031be10c883254c65303c0c0d2a2831c424d97
Hau Linh <haulinhlk@gmail.com>
2020-07-03 09:11:59 +0700

    fix ui config notification tab

--- src/containers/manager/station-auto/station-auto-config-notification/ConfigNotificationTab/index.js
+++ src/containers/manager/station-auto/station-auto-config-notification/ConfigNotificationTab/index.js
@@ -1,24 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Spin } from 'antd'
-import { autobind } from 'core-decorators'
+import { Spin, Collapse } from 'antd'
 import OrganizationApi from 'api/OrganizationApi'
 import createManagerList from 'hoc/manager-list'
-import createLanguageHoc from 'hoc/create-lang'
-import Clearfix from 'components/elements/clearfix'
+import createLanguageHoc, { translate } from 'hoc/create-lang'
 import TableConfig from './TableConfig'
+
+const { Panel } = Collapse
 
 @createManagerList({
   apiList: OrganizationApi.getConfigNotify,
 })
 @createLanguageHoc
-@autobind
 export default class ConfigNotificationTab extends React.Component {
   static propTypes = {
     dataSource: PropTypes.array,
     pagination: PropTypes.object,
-    data: PropTypes.object,
-    onChangeSearch: PropTypes.func,
     isLoading: PropTypes.bool,
   }
 
@@ -26,33 +23,23 @@ export default class ConfigNotificationTab extends React.Component {
     dataSource: [],
   }
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      /* giông cách hoạt động của git */
-
-      cachedData: {} /* commit */,
-      dataSource: [] /* working dir */,
-      dataSourceOriginal: [] /* index */,
-
-      isSave: false,
-
-      isSamplingIndeterminate: true,
-      isSamplingCheckAll: false,
-    }
-  }
-
   render() {
     const { dataSource: configNotifyData, isLoading } = this.props
     return (
       <React.Fragment>
         <Spin spinning={isLoading}>
-          {configNotifyData.map(configNotify => (
-            <React.Fragment key={configNotify.key}>
-              <TableConfig title={configNotify.key} data={configNotify} />
-              <Clearfix height={20} />
-            </React.Fragment>
-          ))}
+          <Collapse 
+          defaultActiveKey={['DATA_STATUS']}
+          >
+            {configNotifyData.map(configNotify => (
+              <Panel
+                header={translate(`configNotify.headerStatus.${configNotify.key}`)}
+                key={configNotify.key}
+              >
+                <TableConfig title={configNotify.key} data={configNotify} />
+              </Panel>
+            ))}
+          </Collapse>
         </Spin>
       </React.Fragment>
     )
