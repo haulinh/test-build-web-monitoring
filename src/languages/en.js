0ebfe7fe8cb956ee5ca53e08d948fe406f9ea478
Huy Cuong <huycuongdn1999@gmail.com>
2020-07-16 11:35:33 +0700

    add giao dien config thoi gian mat tin hieu

--- src/languages/en.js
+++ src/languages/en.js
@@ -1201,7 +1201,7 @@ export default {
       },
       qcvn: {
         label: 'Exceeded Limit',
-        placeholder: 'Standards VN',
+        placeholder: 'Standards',
         error: 'Please Choose Type Of Standards VN',
       },
       tendToExceed: {
@@ -1244,6 +1244,19 @@ export default {
         label: 'Latitude',
         placeholder: 'Input Latitude',
         error: 'Please Input Latitude',
+      },
+      connectionStatus: {
+        label: 'Connection status',
+        time: {
+          options: {
+            minutes: 'Minutes',
+            hours: 'Hours',
+            days: 'Days',
+            months: 'Months',
+            years: 'Years',
+          }
+        },
+        description: 'Config time range to consider as lost connection'
       },
       emails: {
         label: 'Email Address',
