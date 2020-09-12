c05c3b6b822a00213e18539a8b28f44502254796
Huy Cuong <huycuongdn1999@gmail.com>
2020-09-12 15:48:09 +0700

    parse json

--- src/redux/actions/notification.js
+++ src/redux/actions/notification.js
@@ -95,6 +95,7 @@ function updateCountOnNewMsg(num = 1) {
 }
 
 function updateDataSourceOnNewMsg(data) {
+
   return {
     type: NEW_MESSAGE,
     payload: data,
@@ -329,7 +330,7 @@ function _generateNotificationCellByType(rawContent, stationInfo) {
       viewDetail: viewDetailURL,
       aroundAtExceededTime: RawDataURL,
     },
-    measures: rawContent.measures,
+    measures: rawContent.measures ? JSON.parse(rawContent.measures) : [],
     notificationType: rawContent.notificationType,
   }
 
