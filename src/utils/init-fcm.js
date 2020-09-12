11956052a70e7d3de2695842f863fa1a93e670b2
Thằng Khùng <ly.phan@vietan-software.com>
2020-09-12 09:08:10 +0700

    Disable notifications

--- src/utils/init-fcm.js
+++ src/utils/init-fcm.js
@@ -20,4 +20,44 @@ messaging.onMessage(function(payload) {
   console.log('Message code. ', payload)
   // ...
 })
+
+// export const unsubscribeFCM = () => {
+//   navigator.serviceWorker.ready.then(serviceWorkerRegistration => {
+//     serviceWorkerRegistration.pushManager
+//       .getSubscription()
+//       .then(pushSubscription => {
+//         if (!pushSubscription) {
+//         }
+
+//         let subscriptionId = pushSubscription.subscriptionId
+
+//         console.log(subscriptionId, '_____SUB ID_____')
+
+//         pushSubscription
+//           .unsubscribe()
+//           .then(function(successful) {
+//             // pushButton.disabled = false
+//             // pushButton.textContent = 'Enable Push Messages'
+//             // isPushEnabled = false
+//             console.log(successful, '_____DISABLE____')
+//           })
+//           .catch(function(e) {
+//             // We failed to unsubscribe, this can lead to
+//             // an unusual state, so may be best to remove
+//             // the users data from your data store and
+//             // inform the user that you have done so
+
+//             console.log('Unsubscription error: ', e)
+//             // pushButton.disabled = false
+//             // pushButton.textContent = 'Enable Push Messages'
+//           })
+//       })
+//       .catch(e => {
+//         console.error(
+//           'Error thrown while unsubscribing from push messaging.',
+//           e
+//         )
+//       })
+//   })
+// }
 export { messaging }
