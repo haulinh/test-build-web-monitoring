168545573193b70f00b6332d4b182fde2a066ceb
ldquan <ludinhquan@gmail.com>
2020-12-21 15:34:49 +0700

    validate duplicate parameter

--- src/config.js
+++ src/config.js
@@ -32,8 +32,8 @@ export function getConfigApi() {
     // stationFixedDataPoint: 'http://localhost:5018/data-points',
     // stationFixedExportDataPoint: 'http://localhost:5018/export-data-points',
     stationFixedExportDataPoint: c('station-fixed/export-data-points'),
-    stationFixedPoint: c('station-fixed/points'),
-    // stationFixedPoint: 'http://localhost:5018/points',
+    // stationFixedPoint: c('station-fixed/points'),
+    stationFixedPoint: 'http://localhost:5018/points',
     dataStationFixed: c('data-station-fixed'),
 
     stationConfig: c('config/station'),
