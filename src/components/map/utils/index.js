6b14f86db5113cd13a08e67c140d8b70ec81c117
kusinh888 <qui.dinh@vietan-software.com>
2021-06-29 17:35:49 +0700

    fix bug map

--- src/components/map/utils/index.js
+++ src/components/map/utils/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import { GOOGLE_MAP } from 'config'
+import { getApps } from 'config'
 
 export function getGoogleMapProps(lang = 'en') {
+  const key = getApps().googleMapKey
   return {
-    googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAP.KEY}&v=3.exp&language=${lang}&libraries=geometry,drawing,places`,
+    googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${key}&v=3.exp&language=${lang}&libraries=geometry,drawing,places`,
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `100vh` }} />,
     mapElement: <div style={{ height: `100%` }} />,
