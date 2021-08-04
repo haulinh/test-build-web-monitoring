d6641026e60ef8456f0299bdc0bddae1d888c528
kusinh888 <qui.dinh@vietan-software.com>
2021-08-04 12:28:05 +0700

    fix bug

--- src/components/elements/select-province/index.js
+++ src/components/elements/select-province/index.js
@@ -92,6 +92,7 @@ export default class SelectProvince extends PureComponent {
   render() {
     const provinces = this.getProvinces()
     const { fieldValue, value } = this.props
+    let defaultValue = typeof value === 'object' ? value.key : value
     return (
       <Select
         {...this.props}
@@ -100,9 +101,12 @@ export default class SelectProvince extends PureComponent {
         allowClear
         onChange={this.handleOnChange}
         onSearch={this.handleSearch}
-        value={value || this.state.value}
+        value={defaultValue || this.state.value}
         filterOption={false}
       >
+        <Select.Option value="">
+          {translate('dataSearchFrom.form.all')}
+        </Select.Option>
         {this.props.isShowAll && (
           <Select.Option value="">
             {translate('dataSearchFrom.form.all')}
