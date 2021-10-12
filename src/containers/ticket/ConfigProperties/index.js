3b5eeb67e0230f0121e0a46e63826361ae3407e5
haulinh <haulinhlk@gmail.com>
2021-10-12 18:17:24 +0700

    fix role ticket

--- src/containers/ticket/ConfigProperties/index.js
+++ src/containers/ticket/ConfigProperties/index.js
@@ -8,7 +8,8 @@ import { Clearfix } from 'components/elements'
 import CalculateApi from 'api/CalculateApi'
 import createBreadcrumb from 'shared/breadcrumb/hoc'
 import protectRole from 'hoc/protect-role'
-import ROLE from 'constants/role'
+import ROLE, { checkRolePriority } from 'constants/role'
+import { connect } from 'react-redux'
 
 const Breadcrumb = createBreadcrumb()
 
@@ -39,7 +40,9 @@ const i18n = () => ({
   },
 })
 
-@protectRole(ROLE.INCIDENT_CONFIG_PROPERTIES.VIEW)
+@connect(state => ({
+  userInfo: state.auth.userInfo,
+}))
 export default class ConfigProperties extends Component {
   state = {
     visible: false,
@@ -123,7 +126,10 @@ export default class ConfigProperties extends Component {
             },
           ]}
         />
-        {protectRole(ROLE.INCIDENT_CONFIG_PROPERTIES.EDIT)(
+        {checkRolePriority(
+          this.props.userInfo,
+          ROLE.INCIDENT_CONFIG_PROPERTIES.EDIT
+        ) && (
           <ConfigForm
             visible={visible}
             onClose={this.onClose}
@@ -134,6 +140,7 @@ export default class ConfigProperties extends Component {
             showDrawer={this.showDrawer}
           />
         )}
+
         <Clearfix height={16} />
         <ConfigList
           configs={configs}
--- src/containers/ticket/IncidentManagement/TableData.js
+++ src/containers/ticket/IncidentManagement/TableData.js
@@ -8,6 +8,8 @@ import { DD_MM_YYYY_HH_MM } from 'constants/format-date'
 import { withRouter } from 'react-router'
 import slug from 'constants/slug'
 import styled from 'styled-components'
+import { connect } from 'react-redux'
+import role, { checkRolePriority } from 'constants/role'
 
 const TableStyled = styled(Table)`
   .ant-table-row {
@@ -36,103 +38,115 @@ const TooltipCustom = ({ name }) => {
   )
 }
 
-export const TableData = withRouter(
-  ({
-    result = {},
-    setPage = () => {},
-    onSearch = () => {},
-    page,
-    loading,
-    ...otherProps
-  }) => {
-    const columns = [
-      {
-        title: '#',
-        render: (_, __, index) => (
-          <div>{(page - 1) * PAGE_SIZE + (index + 1)}</div>
-        ),
-      },
-      {
-        dataIndex: 'name',
-        width: 200,
-        align: 'center',
-        title: i18n().name,
-        render: value => <TooltipCustom name={value} />,
-      },
-      {
-        dataIndex: 'type',
-        align: 'center',
-        title: i18n().incidentType,
-        render: value => <div>{incidentType()[value]}</div>,
-      },
-      {
-        dataIndex: 'stations',
-        align: 'center',
-        title: t('apiSharingNew.fields.stationKeys'),
-        render: value => {
-          const stationNames = value.map(item => item.name).join(',')
-          return <TooltipCustom name={stationNames} />
+function mapStateToProps(state) {
+  const userInfo = state.auth.userInfo
+  return { userInfo }
+}
+
+export const TableData = connect(mapStateToProps)(
+  withRouter(
+    ({
+      result = {},
+      setPage = () => {},
+      onSearch = () => {},
+      page,
+      loading,
+      userInfo,
+      ...otherProps
+    }) => {
+      const columns = [
+        {
+          title: '#',
+          render: (_, __, index) => (
+            <div>{(page - 1) * PAGE_SIZE + (index + 1)}</div>
+          ),
         },
-      },
-      {
-        dataIndex: 'status',
-        align: 'center',
-        title: i18n().status,
-        render: value => (
-          <div
-            style={{
-              ...omit(value, ['_id, name']),
-              textAlign: 'center',
-              border: `1px solid ${value.color}`,
-              borderRadius: '4px',
-            }}
-          >
-            {value.name}
-          </div>
-        ),
-      },
-      {
-        dataIndex: 'timeStart',
-        align: 'center',
-        defaultSortOrder: 'descend',
-        sorter: (a, b) => new Date(a.timeStart) - new Date(b.timeStart),
-        title: t('avgSearchFrom.selectTimeRange.startTime'),
-        render: value => <div>{moment(value).format(DD_MM_YYYY_HH_MM)}</div>,
-      },
-      {
-        dataIndex: 'timeEnd',
-        align: 'center',
-        title: t('avgSearchFrom.selectTimeRange.endTime'),
-        render: value => (
-          <div>{value && moment(value).format(DD_MM_YYYY_HH_MM)}</div>
-        ),
-      },
-    ]
+        {
+          dataIndex: 'name',
+          width: 200,
+          align: 'center',
+          title: i18n().name,
+          render: value => <TooltipCustom name={value} />,
+        },
+        {
+          dataIndex: 'type',
+          align: 'center',
+          title: i18n().incidentType,
+          render: value => <div>{incidentType()[value]}</div>,
+        },
+        {
+          dataIndex: 'stations',
+          align: 'center',
+          title: t('apiSharingNew.fields.stationKeys'),
+          render: value => {
+            const stationNames = value.map(item => item.name).join(',')
+            return <TooltipCustom name={stationNames} />
+          },
+        },
+        {
+          dataIndex: 'status',
+          align: 'center',
+          title: i18n().status,
+          render: value => (
+            <div
+              style={{
+                ...omit(value, ['_id, name']),
+                textAlign: 'center',
+                border: `1px solid ${value.color}`,
+                borderRadius: '4px',
+              }}
+            >
+              {value.name}
+            </div>
+          ),
+        },
+        {
+          dataIndex: 'timeStart',
+          align: 'center',
+          defaultSortOrder: 'descend',
+          sorter: (a, b) => new Date(a.timeStart) - new Date(b.timeStart),
+          title: t('avgSearchFrom.selectTimeRange.startTime'),
+          render: value => <div>{moment(value).format(DD_MM_YYYY_HH_MM)}</div>,
+        },
+        {
+          dataIndex: 'timeEnd',
+          align: 'center',
+          title: t('avgSearchFrom.selectTimeRange.endTime'),
+          render: value => (
+            <div>{value && moment(value).format(DD_MM_YYYY_HH_MM)}</div>
+          ),
+        },
+      ]
 
-    const handleOnChangePagination = (page, pageSize) => {
-      setPage(page)
-      onSearch()
+      const handleOnChangePagination = (page, pageSize) => {
+        setPage(page)
+        onSearch()
+      }
+
+      return (
+        <TableStyled
+          columns={columns}
+          onRow={(record, rowIndex) => {
+            return {
+              onClick: event => {
+                if (
+                  checkRolePriority(userInfo, role.INCIDENT_MANAGEMENT.EDIT)
+                ) {
+                  otherProps.history.push(
+                    `${slug.ticket.incidentEdit}/${record._id}`
+                  )
+                }
+              }, // click row
+            }
+          }}
+          dataSource={get(result, 'data', [])}
+          pagination={{
+            onChange: handleOnChangePagination,
+            total: get(result, 'pagination.total'),
+          }}
+          loading={loading}
+        />
+      )
     }
-
-    return (
-      <TableStyled
-        columns={columns}
-        onRow={(record, rowIndex) => {
-          return {
-            onClick: event => {
-              otherProps.history.push(
-                `${slug.ticket.incidentEdit}/${record._id}`
-              )
-            }, // click row
-          }
-        }}
-        dataSource={get(result, 'data', [])}
-        pagination={{
-          onChange: handleOnChangePagination,
-          total: get(result, 'pagination.total'),
-        }}
-        loading={loading}
-      />
-    )
-  }
+  )
 )
