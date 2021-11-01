57a19ea34adefda9ee342e6b037f981c20dfe6d2
haulinh <haulinhlk@gmail.com>
2021-11-01 15:37:31 +0700

    apply qcvn onChange

--- src/containers/manager/station-fixed-report/search-form/SelectQCVN.js
+++ src/containers/manager/station-fixed-report/search-form/SelectQCVN.js
@@ -3,14 +3,24 @@ import { translate as t } from 'hoc/create-lang'
 import React from 'react'
 import { FormItemStyled, FIELDS } from './index'
 
-const SelectQCVNForm = ({ form, setStandardVNObject }) => {
+const SelectQCVNForm = ({
+  form,
+  setStandardVNObject,
+  handleOnSubmit = () => {},
+}) => {
   const changeQCVN = value => {
     setStandardVNObject(value)
+    handleOnSubmit()
+  }
+
+  const handleOnChange = () => {
+    handleOnSubmit()
   }
 
   return (
     <FormItemStyled label="Quy chuẩn">
       {form.getFieldDecorator(FIELDS.STANDARDS_VN, {
+        onChange: handleOnChange,
         rules: [
           {
             required: false,
