67832d328a82d7138e579f511b23deb0c123bdf3
haulinh <haulinhlk@gmail.com>
2021-11-17 10:20:31 +0700

    split config advanced

--- src/containers/qa-qc/config/index.js
+++ src/containers/qa-qc/config/index.js
@@ -2,7 +2,7 @@ import { Clearfix } from 'components/layouts/styles'
 import PageContainer from 'layout/default-sidebar-layout/PageContainer'
 import React from 'react'
 import Breadcrumb from '../breadcrumb'
-import ConfigQaqcAdvancedTab from './ConfigQaqcAdvancedTab'
+import ConfigQaqcAdvancedTab from './ConfigQaqcAdvanced'
 import ConfigQaqcBasic from './ConfigQaqcBasic'
 
 export default class QAQC_Config extends React.Component {
@@ -15,12 +15,10 @@ export default class QAQC_Config extends React.Component {
     }
   }
 
-
   render() {
     if (this.props.isDrawer) {
       return this.renderContent()
     }
-
 
     return (
       <PageContainer backgroundColor={'#fafbfb'}>
