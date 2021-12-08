28c24114af479cf0798ad4e7dfe69b2fecb2d511
hoang.vo@ilotusland.com <stomper013@gmai.com>
2021-12-08 17:29:34 +0700

    fix(ModalExport): update

--- src/components/elements/modal-lang-export/index.js
+++ src/components/elements/modal-lang-export/index.js
@@ -1,9 +1,9 @@
-import { Modal, Radio } from 'antd';
+import { Button, Modal, Radio, Row } from 'antd';
 import React from 'react';
 import { translate as t } from 'hoc/create-lang'
 
 function i18n() {
-  return{
+  return {
     title: t('modalExportLang.title'),
     content: t('modalExportLang.content'),
     language: {
@@ -30,20 +30,29 @@ export default class ModalLangExport extends React.Component {
     const { showModal, handleOkModal, handleCancelModal, onChangeModal, langExport } = this.props
     return (
       <Modal
-        title={i18n().title}
+        width="360px"
+        closable={false}
+        footer={false}
         visible={showModal}
         onOk={handleOkModal}
         onCancel={handleCancelModal}
         okText={i18n().button.ok}
         cancelText={i18n().button.cancel}
+        centered
       >
+        <h4>{i18n().title}</h4>
         <p>{i18n().content}</p>
         {radioOption.map(item => (
           <Radio.Group onChange={onChangeModal} value={langExport} >
             <Radio value={item.value}>{item.label}</Radio>
           </Radio.Group>
         ))}
-      </Modal>
+
+        <Row type="flex" justify="end" style={{ gap: 13, marginTop: 20 }}>
+          <Button type="primary">{i18n().button.ok}</Button>
+          <Button>{i18n().button.cancel}</Button>
+        </Row>
+      </Modal >
     )
   }
 }
