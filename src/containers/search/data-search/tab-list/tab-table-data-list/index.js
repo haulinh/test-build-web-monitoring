279dae1b00a4241458e46607785542c8c6e1ac2a
Kinh Bach <kinh.bach@ilotusland.com>
2022-05-20 15:55:19 +0700

    move function outsite

--- src/containers/search/data-search/tab-list/tab-table-data-list/index.js
+++ src/containers/search/data-search/tab-list/tab-table-data-list/index.js
@@ -152,6 +152,15 @@ export default class TableDataList extends React.Component {
     const { measuringList } = this.props
     const standardsSelected = this.getStandardsSelected()
 
+    const renderStandard = measure => {
+      const { minLimit, maxLimit } = measure || {}
+      if ((minLimit || minLimit === 0) && (maxLimit || maxLimit === 0))
+        return [minLimit, maxLimit].join('-')
+      if (minLimit || minLimit === 0) return `≥ ${minLimit}`
+      if (maxLimit || maxLimit === 0) return `≤ ${maxLimit}`
+      return '-'
+    }
+
     const renderFooter = () => {
       if (!isEmpty(standardsSelected)) {
         return (
@@ -165,18 +174,6 @@ export default class TableDataList extends React.Component {
               const expiredTime = standard.expired
                 ? moment(standard.expired).format('DD/MM/YYYY')
                 : '-'
-              const renderStandard = measure => {
-                const { minLimit, maxLimit } = measure || {}
-                if (
-                  (minLimit || minLimit === 0) &&
-                  (maxLimit || maxLimit === 0)
-                )
-                  return [minLimit, maxLimit].join('-')
-                if (minLimit || minLimit === 0) return `≥ ${minLimit}`
-                if (maxLimit || maxLimit === 0) return `≤ ${maxLimit}`
-                return '-'
-              }
-
               return (
                 <tr className="ant-table-row">
                   <td />
