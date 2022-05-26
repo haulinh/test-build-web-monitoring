4e0c08cea6dc413dfd008baa53ae02fa5d5c3b86
kinhbach <kinh.bach@ilotusland.com>
2022-05-26 14:41:25 +0700

    remove state

--- src/containers/alarm/AlarmSetting/ManagementAlarm/index.js
+++ src/containers/alarm/AlarmSetting/ManagementAlarm/index.js
@@ -20,7 +20,6 @@ export default class ManagementAlarm extends Component {
   state = {
     users: [],
     roles: [],
-    alarmList: [],
     isLoading: false,
   }
   componentDidMount = async () => {
