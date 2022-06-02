ac3c596110b1bbd4f836a378446d3748d9d8cf52
kinhbach <kinh.bach@ilotusland.com>
2022-06-02 09:45:06 +0700

    fix warning icon, change name icon

--- src/assets/icons/Element4.js
+++ src/assets/icons/Element4.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const AdvancedIcon = () => {
+export const Element4 = () => {
   return (
     <svg
       width="21"
@@ -12,30 +12,30 @@ export const AdvancedIcon = () => {
       <path
         d="M18.8333 9.08341V3.41675C18.8333 2.16675 18.3 1.66675 16.975 1.66675H13.6083C12.2833 1.66675 11.75 2.16675 11.75 3.41675V9.08341C11.75 10.3334 12.2833 10.8334 13.6083 10.8334H16.975C18.3 10.8334 18.8333 10.3334 18.8333 9.08341Z"
         stroke="currentColor"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M18.8333 16.5833V15.0833C18.8333 13.8333 18.3 13.3333 16.975 13.3333H13.6083C12.2833 13.3333 11.75 13.8333 11.75 15.0833V16.5833C11.75 17.8333 12.2833 18.3333 13.6083 18.3333H16.975C18.3 18.3333 18.8333 17.8333 18.8333 16.5833Z"
         stroke="currentColor"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M9.25002 10.9167V16.5834C9.25002 17.8334 8.71669 18.3334 7.39169 18.3334H4.02502C2.70002 18.3334 2.16669 17.8334 2.16669 16.5834V10.9167C2.16669 9.66675 2.70002 9.16675 4.02502 9.16675H7.39169C8.71669 9.16675 9.25002 9.66675 9.25002 10.9167Z"
         stroke="currentColor"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M9.25002 3.41675V4.91675C9.25002 6.16675 8.71669 6.66675 7.39169 6.66675H4.02502C2.70002 6.66675 2.16669 6.16675 2.16669 4.91675V3.41675C2.16669 2.16675 2.70002 1.66675 4.02502 1.66675H7.39169C8.71669 1.66675 9.25002 2.16675 9.25002 3.41675Z"
         stroke="currentColor"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   )
--- src/assets/icons/InfoIcon.js
+++ src/assets/icons/InfoIcon.js
@@ -12,23 +12,23 @@ export const InfoIcon = () => {
       <path
         d="M8.49998 14.6666C12.1666 14.6666 15.1666 11.6666 15.1666 7.99992C15.1666 4.33325 12.1666 1.33325 8.49998 1.33325C4.83331 1.33325 1.83331 4.33325 1.83331 7.99992C1.83331 11.6666 4.83331 14.6666 8.49998 14.6666Z"
         stroke="#A2A7B3"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M8.5 5.33325V8.66659"
         stroke="#A2A7B3"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M8.49634 10.6667H8.50233"
         stroke="#A2A7B3"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   )
